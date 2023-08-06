6c0a6636337d3a95b6b9ff42de13b90b39c05325
Neil Francis Ramirez Hipona <enef.devops@protonmail.ch>
2023-08-07 00:19:03 +0800

    Create view model classes

---
+++ ViewModels/MyBaseTagItemViewModel.swift
@@ -0,0 +1,34 @@
+//
+//  MyBaseTagItemViewModel.swift
+//  MyTagCollection
+//
+//  Created by Neil Francis Hipona on 8/6/23.
+//
+
+import Foundation
+
+public
+struct MyBaseTagItemViewModel: MyTagItemProtocol {
+    public var modelView: MyBaseTagItemView.Type {
+        MyBaseTagItemView.self
+    }
+    
+    public let identifier: String
+    public let model: MyTagItemModelProtocol
+    public let attribute: MyTagItemAttributeProtocol
+    public var section: Int // will be set dynamically upon layout calculations
+    public var isSelected: Bool
+    
+    public
+    init(identifier: String,
+         model: MyTagItemModelProtocol,
+         attribute: MyTagItemAttributeProtocol,
+         section: Int = 0,
+         isSelected: Bool = false) {
+        self.identifier = identifier
+        self.model = model
+        self.attribute = attribute
+        self.section = section
+        self.isSelected = isSelected
+    }
+}
